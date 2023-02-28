80f0d686e98a6280e8c39f88d514127951864393
Erich Querner <querner@interactive-scape.com>
2023-02-28 10:57:42 +0100

    auto process messages in demo application

--- src/TuioNet.Demo/Program.cs
+++ src/TuioNet.Demo/Program.cs
@@ -7,14 +7,13 @@ class Program
 {
     private static void Main(string[] args)
     {
-        var tuioReceiver = new UdpTuioReceiver(3333, false);
+        var tuioReceiver = new UdpTuioReceiver(3333, true);
         var tuioClient = new Tuio11Client(tuioReceiver);
         tuioClient.AddTuioListener(new TuioListener());
         Console.WriteLine("Connect...");
         tuioClient.Connect();
         while (true)
         {
-            tuioReceiver.ProcessMessages();
             if (!Console.KeyAvailable) continue;
             var pressedKey = Console.ReadKey().Key;
             if (pressedKey == ConsoleKey.Q) break;
