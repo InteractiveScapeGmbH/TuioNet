0ddeb55e9b274529ef0d7ffe144162e5c2f2b907
Erich Querner <querner@interactive-scape.com>
2023-05-08 17:42:47 +0200

    update demo program

--- src/TuioNet.Demo/TuioListener.cs
+++ src/TuioNet.Demo/TuioListener.cs
@@ -3,54 +3,35 @@ using TuioNet.Tuio11;
 
 namespace TuioNet.Demo;
 
-public class TuioListener : ITuio11Listener
+public class TuioListener : ITuio11CursorListener, ITuio11ObjectListener
 {
-    public void AddTuioObject(Tuio11Object tuio11Object)
+    public void AddObject(Tuio11Object tuio11Object)
     {
         Console.WriteLine($"New object added -> ID: {tuio11Object.SymbolId}");
     }
 
-    public void UpdateTuioObject(Tuio11Object tuio11Object)
+    public void UpdateObject(Tuio11Object tuio11Object)
     {
         Console.WriteLine($"Object {tuio11Object.SymbolId} -> Position: {tuio11Object.Position}, Angle: {tuio11Object.Angle}");
     }
 
-    public void RemoveTuioObject(Tuio11Object tuio11Object)
+    public void RemoveObject(Tuio11Object tuio11Object)
     {
         Console.WriteLine($"Object {tuio11Object.SymbolId} removed");
     }
 
-    public void AddTuioCursor(Tuio11Cursor tuio11Cursor)
+    public void AddCursor(Tuio11Cursor tuio11Cursor)
     {
         Console.WriteLine($"New cursor added -> ID: {tuio11Cursor.CursorId}");
     }
 
-    public void UpdateTuioCursor(Tuio11Cursor tuio11Cursor)
+    public void UpdateCursor(Tuio11Cursor tuio11Cursor)
     {
         Console.WriteLine($"Cursor {tuio11Cursor.CursorId} -> Position: {tuio11Cursor.Position}");
     }
 
-    public void RemoveTuioCursor(Tuio11Cursor tuio11Cursor)
+    public void RemoveCursor(Tuio11Cursor tuio11Cursor)
     {
         Console.WriteLine($"Cursor {tuio11Cursor.CursorId} removed");
-    }
-
-    public void AddTuioBlob(Tuio11Blob tuio11Blob)
-    {
-        Console.WriteLine($"New blob added -> ID: {tuio11Blob.BlobId}");
-    }
-
-    public void UpdateTuioBlob(Tuio11Blob tuio11Blob)
-    {
-        Console.WriteLine($"Blob {tuio11Blob.BlobId} -> Position: {tuio11Blob.Position}, Angle: {tuio11Blob.Angle}, Area: {tuio11Blob.Area}");
-    }
-
-    public void RemoveTuioBlob(Tuio11Blob tuio11Blob)
-    {
-        Console.WriteLine($"Blob {tuio11Blob.BlobId} removed.");
-    }
-
-    public void Refresh(TuioTime tuioTime)
-    {
     }
 }
