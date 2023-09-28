cf4c6e92ae6b19aac5ed69eea9a33ccff02f6148
Erich Querner <querner@interactive-scape.com>
2023-09-28 08:46:16 +0200

    add general interface if one wants to listen to all possible tuio messages

---
+++ src/TuioNet/Tuio11/ITuio11Listener.cs
@@ -0,0 +1,27 @@
+﻿using TuioNet.Common;
+
+namespace TuioNet.Tuio11
+{
+    public interface ITuio11Listener
+    {
+        public void AddTuioObject(Tuio11Object tuio11Object);
+        
+        public void UpdateTuioObject(Tuio11Object tuio11Object);
+        
+        public void RemoveTuioObject(Tuio11Object tuio11Object);
+        
+        public void AddTuioCursor(Tuio11Cursor tuio11Cursor);
+        
+        public void UpdateTuioCursor(Tuio11Cursor tuio11Cursor);
+        
+        public void RemoveTuioCursor(Tuio11Cursor tuio11Cursor);
+        
+        public void AddTuioBlob(Tuio11Blob tuio11Blob);
+        
+        public void UpdateTuioBlob(Tuio11Blob tuio11Blob);
+        
+        public void RemoveTuioBlob(Tuio11Blob tuio11Blob);
+        
+        public void Refresh(TuioTime tuioTime);
+    }
+}
