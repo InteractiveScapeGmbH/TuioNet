443925857e0a18e0f9294f23e35f585e813fd41f
Erich Querner <querner@interactive-scape.com>
2023-10-06 16:48:21 +0200

    remove casts

--- src/TuioNet/Tuio11/Tuio11Cursor.cs
+++ src/TuioNet/Tuio11/Tuio11Cursor.cs
@@ -22,7 +22,7 @@ namespace TuioNet.Tuio11
 
         internal void Update(TuioTime currentTime, Vector2 position, Vector2 velocity, float acceleration)
         {
-            var isCalculateSpeeds = (position.X != ((Tuio11Point)this).Position.X && velocity.X == 0) || (position.Y != ((Tuio11Point)this).Position.Y && velocity.Y == 0);
+            var isCalculateSpeeds = (position.X != Position.X && velocity.X == 0) || (position.Y != Position.Y && velocity.Y == 0);
             UpdateContainer(currentTime, position, velocity, acceleration, isCalculateSpeeds);
         }
     }
