994f31a3b70ee9cb27a2027a1f69426610987d4b
Erich Querner <querner@interactive-scape.com>
2023-10-20 16:50:49 +0200

    update demo for tuio 11

--- src/TuioNet.Demo/Program.cs
+++ src/TuioNet.Demo/Program.cs
@@ -8,8 +8,8 @@ class Program
 {
     private static void Main(string[] args)
     {
-        var tuioClient = new Tuio20Client(TuioConnectionType.UDP);
-        tuioClient.AddTuioListener(new Listener20());
+        var tuioClient = new Tuio11Client(TuioConnectionType.UDP);
+        tuioClient.AddTuioListener(new Listener11());
         Console.WriteLine("Connect...");
         tuioClient.Connect();
         while (true)
@@ -23,7 +23,7 @@ class Program
     }
 }
 
-public class Listener20 : ITuio20Listener
+public class Listener11 : ITuio11Listener
 {
     public void TuioAdd(Tuio20Object tuio20Object)
     {
@@ -52,6 +52,54 @@ public class Listener20 : ITuio20Listener
     public void TuioRefresh(TuioTime tuioTime)
     {
         // throw new NotImplementedException();
+    }
+
+    public void AddTuioObject(Tuio11Object tuio11Object)
+    {
+        Console.WriteLine($"Object {tuio11Object.SymbolId} added.");
+    }
+
+    public void UpdateTuioObject(Tuio11Object tuio11Object)
+    {
+        Console.WriteLine($"Cursor {tuio11Object.SymbolId} -> Position: {tuio11Object.Position}, Velocity: {tuio11Object.Velocity}, Speed: {tuio11Object.Speed}, Time: {tuio11Object.CurrentTime}");
+    }
+
+    public void RemoveTuioObject(Tuio11Object tuio11Object)
+    {
+        Console.WriteLine($"Object {tuio11Object.SymbolId} removed.");
+
+    }
+
+    public void AddTuioCursor(Tuio11Cursor tuio11Cursor)
+    {
+        Console.WriteLine($"Cursor {tuio11Cursor.SessionId} added.");
+
+    }
+
+    public void UpdateTuioCursor(Tuio11Cursor tuio11Cursor)
+    {
+        Console.WriteLine($"Cursor {tuio11Cursor.SessionId} -> Position: {tuio11Cursor.Position}, Velocity: {tuio11Cursor.Velocity}, Speed: {tuio11Cursor.Speed}, Time: {tuio11Cursor.CurrentTime}");
+    }
+
+    public void RemoveTuioCursor(Tuio11Cursor tuio11Cursor)
+    {
+        Console.WriteLine($"Cursor {tuio11Cursor.SessionId} removed.");
+    }
+
+    public void AddTuioBlob(Tuio11Blob tuio11Blob)
+    {
+    }
+
+    public void UpdateTuioBlob(Tuio11Blob tuio11Blob)
+    {
+    }
+
+    public void RemoveTuioBlob(Tuio11Blob tuio11Blob)
+    {
+    }
+
+    public void Refresh(TuioTime tuioTime)
+    {
     }
 }
 
