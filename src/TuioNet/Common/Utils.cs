185ea2514185f4f182fc51f0b2875fa68a731a68
Erich Querner <querner@interactive-scape.com>
2024-07-10 18:13:04 +0200

    add util methods to pack/unpack vector2 in/from uint

---
+++ src/TuioNet/Common/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace TuioNet.Common;
+
+public class Utils
+{
+    public static uint FromDimension(Vector2 dimension)
+    {
+        var width = (ushort)MathF.Round(dimension.X);
+        var height = (ushort)MathF.Round(dimension.Y);
+        return ((uint)width << 16) | height;
+    }
+
+    public static Vector2 ToDimension(uint encoded)
+    {
+        var width = (ushort)(encoded >> 16);
+        var height = (ushort)(encoded & 0xFFFF);
+        return new Vector2(width, height);
+    }
+}
