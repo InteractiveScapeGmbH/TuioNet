adc210c5a376df870f7675a0e82e6fe17805db00
Erich Querner <querner@interactive-scape.com>
2025-04-22 16:16:55 +0200

    parse commandline arguments for ip, port, loglevel, tuio version and connection type

--- src/TuioNet.Demo/Program.cs
+++ src/TuioNet.Demo/Program.cs
@@ -7,26 +7,38 @@ namespace TuioNet.Demo;
 
 class Program
 {
+    public class Options
+    {
+        [Option('i', "ip", HelpText = "Set the ip address of the tuio sender.", Default = "127.0.0.1")]
+        public string IpAddress { get; set; }
+        [Option('p', "port", HelpText = "Set the port.", Default = 3333)]
+        public int Port { get; set; }
+        [Option('l', "logLevel", HelpText = "Set the minimum log level [Trace, Debug, Information, Warning, Error, Critical, None].", Default = LogLevel.Information)]
+        public LogLevel LogLevel { get; set; }
+        [Option('v', "tuioVersion", HelpText = "Set the tuio version [Tuio11, Tuio20].", Default = TuioVersion.Tuio11)]
+        public TuioVersion TuioVersion { get; set; }
+        [Option('c', "connection", HelpText = "Set the connection type [UDP, Websocket].", Default = TuioConnectionType.UDP)]
+        public TuioConnectionType ConnectionType { get; set; }
+    }
+    
     private static void Main(string[] args)
     {
-        using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
-        ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
-        // Setup TUIO version and connection type
-        var tuioVersion = TuioVersion.Tuio11;
-        var connectionType = TuioConnectionType.Websocket;
-        var ipAddress = "127.0.0.1";
-        var port = 3333;
-        using var tuioSession = new TuioSession(logger, tuioVersion, connectionType, ipAddress, port);
-        AddCallbacks(tuioSession);
-        Console.WriteLine("Connect...");
-        while (true)
+        Parser.Default.ParseArguments<Options>(args).WithParsed(option =>
         {
-            if (!Console.KeyAvailable) continue;
-            var pressedKey = Console.ReadKey().Key;
-            if (pressedKey == ConsoleKey.Q) break;
-        }
-        Console.WriteLine("Disconnect...");
-        RemoveCallbacks(tuioSession);
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+            using var tuioSession = new TuioSession(logger, option.TuioVersion, option.ConnectionType, option.IpAddress, option.Port);
+            AddCallbacks(tuioSession);
+            Console.WriteLine("Connect...");
+            while (true)
+            {
+                if (!Console.KeyAvailable) continue;
+                var pressedKey = Console.ReadKey().Key;
+                if (pressedKey == ConsoleKey.Q) break;
+            }
+            Console.WriteLine("Disconnect...");
+            RemoveCallbacks(tuioSession);
+        });
     }
 
     private static void AddCallbacks(TuioSession tuioSession)
