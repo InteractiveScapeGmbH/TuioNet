4f171fd5b00027a88a458232dd3a4cfd9e6b2add
Erich Querner <querner@interactive-scape.com>
2026-01-15 17:33:17 +0100

    implement tuio transmitter and use it in demo. there is no tuio data generator right now.

--- TuioNet.ServerDemo/Program.cs
+++ TuioNet.ServerDemo/Program.cs
@@ -1,3 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Numerics;
+using CommandLine;
+using Microsoft.Extensions.Logging;
+using TuioNet.Common;
+using TuioNet.Server;
+using TuioNet.ServerDemo;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    public class Options
+    {
+        [Option('i', "ip", HelpText = "Set the ip address of the tuio sender.", Default = "127.0.0.1")]
+        public string IpAddress { get; set; }
+        [Option('p', "port", HelpText = "Set the port.", Default = 3333)]
+        public int Port { get; set; }
+        [Option('l', "logLevel", HelpText = "Set the minimum log level [Trace, Debug, Information, Warning, Error, Critical, None].", Default = LogLevel.Information)]
+        public LogLevel LogLevel { get; set; }
+        [Option('v', "tuioVersion", HelpText = "Set the tuio version [Tuio11, Tuio20].", Default = TuioVersion.Tuio11)]
+        public TuioVersion TuioVersion { get; set; }
+        [Option('c', "connection", HelpText = "Set the connection type [UDP, Websocket].", Default = TuioConnectionType.UDP)]
+        public TuioConnectionType ConnectionType { get; set; }
+        [Option('s', "sourceName", HelpText = "Set the source name for the Tuio Sender.", Default = "TuioServerDemo")]
+        public string SourceName { get; set; }
+        [Option('r', "resolution", HelpText = "Set the screen resolution as [x,y]. Only valid for Tuio 2.", Default = "1280,720")]
+        public string Resolution { get; set; }
+    }
+    
+    
+    
+    private static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args).WithParsed(option =>
+        {
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+
+            var resolutionParts = option.Resolution.Split(',');
+            var resolution = new Vector2(float.Parse(resolutionParts[0].Trim()),
+                float.Parse(resolutionParts[1].Trim()));
+
+            ITuioServer server = option.ConnectionType switch
+            {
+                TuioConnectionType.Websocket => new WebsocketServer(logger),
+                TuioConnectionType.UDP => new UdpServer(),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            ITuioManager tuioManager = option.TuioVersion switch
+            {
+                TuioVersion.Tuio11 => new Tuio11Manager(option.SourceName),
+                TuioVersion.Tuio20 => new Tuio20Manager(option.SourceName, resolution),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            var transmitter = new TuioTransmitter(server, tuioManager, logger);
+            transmitter.Open(IPAddress.Parse(option.IpAddress), option.Port);
+            transmitter.Send();
+            transmitter.Close();
+            
+        });
+
+    }
+}
---
+++ TuioNet.ServerDemo/TuioTransmitter.cs
@@ -0,0 +1,55 @@
+using System.Net;
+using Microsoft.Extensions.Logging;
+using TuioNet.Server;
+
+namespace TuioNet.ServerDemo;
+
+public class TuioTransmitter
+{
+    private readonly ITuioServer _server;
+    private readonly ITuioManager _manager;
+    private bool _isInitialized;
+    private readonly ILogger _logger;
+
+    public TuioTransmitter(ITuioServer server, ITuioManager manager, ILogger logger)
+    {
+        _server = server;
+        _manager = manager;
+        _logger = logger;
+        _isInitialized = false;
+    }
+
+    public void Open(IPAddress ipAddress, int port)
+    {
+        _server.Start(ipAddress, port);
+        _isInitialized = true;
+    }
+
+    public void Send()
+    {
+        _manager.Update();
+        try
+        {
+            _server.Send(_manager.FrameBundle.BinaryData);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError("Could not send Tuio message: {e}", exception);
+        }
+    }
+
+    public void Close()
+    {
+        _isInitialized = false;
+        _manager.Quit();
+        try
+        {
+            _server.Send(_manager.FrameBundle.BinaryData);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError("Could not send last data: {e}", exception);
+        }
+        _server.Stop();
+    }
+}
