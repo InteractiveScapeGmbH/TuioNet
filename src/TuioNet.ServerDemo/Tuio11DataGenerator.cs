d07b9e6db8e88bb105054ad07aa4984d7380e101
Erich Querner <querner@interactive-scape.com>
2026-01-16 15:33:02 +0100

    add object. make fields const/readonly and remove manager field since it is only used in constructor

--- src/TuioNet.ServerDemo/Tuio11DataGenerator.cs
+++ src/TuioNet.ServerDemo/Tuio11DataGenerator.cs
@@ -7,27 +7,30 @@ namespace TuioNet.ServerDemo;
 
 public class Tuio11DataGenerator : ITuioDataGenerator
 {
-    private Tuio11Manager _manager;
     private readonly Tuio11Cursor _cursor;
     private readonly Tuio11Object _object;
-    private Vector2 _center = new Vector2(0.5f, 0.5f);
-    private float _touchRadius = 0.4f;
-    private float _objectRadius = 0.2f;
+    private readonly Vector2 _center = new Vector2(0.5f, 0.5f);
+    private const float TouchRadius = 0.4f;
+    private const float ObjectRadius = 0.2f;
     private float _touchAngle = 0f;
     private float _objectAngle = 0f;
     
     public Tuio11DataGenerator(Tuio11Manager manager)
     {
-        _manager = manager;
-        _cursor = new Tuio11Cursor(TuioTime.GetSystemTime(), _manager.CurrentSessionId, 0, Vector2.Zero, Vector2.Zero, 0f);
-        _manager.AddCursor(_cursor);
+        _cursor = new Tuio11Cursor(TuioTime.GetSystemTime(), manager.CurrentSessionId, 0, Vector2.Zero, Vector2.Zero, 0f);
+        manager.AddCursor(_cursor);
+        _object = new Tuio11Object(TuioTime.GetSystemTime(), manager.CurrentSessionId, 5, Vector2.Zero, 0,
+            Vector2.Zero, 0, 0, 0);
+        manager.AddObject(_object);
     }
 
     public void Update(float deltaTime)
     {
         var time = TuioTime.GetSystemTime();
-        var position = MoveOnCircle(_touchRadius, 0.2f, deltaTime, ref _touchAngle);
-        _cursor.Update(time, position);
+        var cursorPosition = MoveOnCircle(TouchRadius, 0.2f, deltaTime, ref _touchAngle);
+        var objectPosition = MoveOnCircle(ObjectRadius, 0.5f, deltaTime, ref _objectAngle);
+        _cursor.Update(time, cursorPosition);
+        _object.Update(time, objectPosition, _objectAngle);
     }
 
     private Vector2 MoveOnCircle(float radius, float angularSpeed, float deltaTime, ref float angle)
